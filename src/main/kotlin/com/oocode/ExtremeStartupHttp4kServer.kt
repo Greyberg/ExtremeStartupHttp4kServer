e8d2e6039e065fa49a317909653b269792522780
Rose Taylor-Garratt <rtayl397@ford.com>
2024-07-25 11:18:50 +0100

    Some stuff

--- src/main/kotlin/com/oocode/ExtremeStartupHttp4kServer.kt
+++ src/main/kotlin/com/oocode/ExtremeStartupHttp4kServer.kt
@@ -32,6 +32,7 @@ private object HomePage {
 <html>
     <body>
     <h1>Extreme startup player (http4k)</h1>
+    <p>wassup?!</p>
     <p>This is a minimal web app starting point for <a href='https://extreme-startup.fly.dev/'>Extreme startup</a></p>
     </body>
 </html>""".trim()
